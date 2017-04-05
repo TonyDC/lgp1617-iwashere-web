0afd757f39a3cc65ec98fc253ed2885fdf97cc87
jmepg <j.estrada.gouveia@gmail.com>
2017-04-05 14:46:15 +0100

    login mockup

--- src/js/user/Login.js
+++ src/js/user/Login.js
@@ -83,21 +83,11 @@ export default class Login extends Component {
 
                     <div className="main-login main-center">
                         <form className="form-horizontal">
-                            <div className="form-group">
-                                <label htmlFor="name" className="cols-sm-2 control-label">Username</label>
-                                <div className="cols-sm-10">
-                                    <div className="input-group" onSubmit={this.loginUser.bind(this)}>
-                                        <span className="input-group-addon"><i className="fa fa-user fa" aria-hidden="true"></i></span>
-                                        <input type="text" className="form-control" name="name" id="name" placeholder="Enter your Name" />
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="form-group">
+                              <div className="form-group">
                                 <label htmlFor="email" className="cols-sm-2 control-label">Your Email</label>
                                 <div className="cols-sm-10">
                                     <div className="input-group">
-                                        <span className="input-group-addon"><i className="fa fa-envelope fa" aria-hidden="true"></i></span>
+                                        <span className="input-group-addon"><i className="fa fa-envelope fa" aria-hidden="true"/></span>
                                         <input type="text" className="form-control" name="email" id="email" placeholder="Enter your Email" onChange={this.handleEmail.bind(this)}/>
                                     </div>
                                 </div>
