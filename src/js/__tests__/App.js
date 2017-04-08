81e831f7fdb76fb0d7c8fe8e9eb38d635f4ef367
A. David Casimiro <TonyDC@users.noreply.github.com>
2017-04-08 21:05:52 +0100

    Login loading screen correction (#10)

--- src/js/__tests__/App.js
+++ src/js/__tests__/App.js
@@ -6,6 +6,14 @@ import App from '../App';
 const EXPECTED_NR_CHILDREN = 1;
 const context = {
     store: {
+        getState: () => {
+            return {
+                userStatus: {
+                    isLogged: false,
+                    userInfo: null
+                }
+            };
+        },
         subscribe: (callback) => {
             return typeof callback === 'function';
         }
@@ -13,7 +21,7 @@ const context = {
 };
 const wrapper = mount(<App/>, { context });
 
-describe('(Component) MyComponent', () => {
+describe('(Component) App component', () => {
     it('renders without crashing', () => {
         expect(wrapper).toHaveLength(EXPECTED_NR_CHILDREN);
     });
