d6afda3c5d6561fb33222439575eb62bec16934c
TonyDC <up201305244@gcloud.fe.up.pt>
2017-04-12 17:44:38 +0100

    Circular menu

--- src/js/components/map/Map.js
+++ src/js/components/map/Map.js
@@ -1,21 +1,42 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import GoogleMapReact from 'google-map-react';
+import { Button } from 'react-bootstrap';
+import MotionMenu from 'react-motion-menu';
 
 import Pin from './Pin';
 
 import 'styles/utils.scss';
 
+/*
+ // 'apiKey' prop
+ <GoogleMapReact defaultCenter={this.props.center} defaultZoom={this.props.zoom}>
+ <Pin lat={41.14792237} lng={-8.61129427}>
+ <Button><MotionMenu type="circle" margin={120} >
+ <Button>This is a button</Button>
+ </MotionMenu></Button>
+ </Pin>
+ </GoogleMapReact>
+ */
+
 export default class Map extends Component {
 
     render() {
         return (
-            // 'apiKey' prop
-            <GoogleMapReact defaultCenter={this.props.center} defaultZoom={this.props.zoom}>
-                <Pin lat={41.14792237} lng={-8.61129427} text="Hello!">
-                    <button>This is a button</button>
-                </Pin>
-            </GoogleMapReact>
+
+                <GoogleMapReact defaultCenter={this.props.center} defaultZoom={this.props.zoom}>
+                    <Pin lat={41.14792237} lng={-8.61129427}>
+                        <MotionMenu type="circle" margin={120} x={0} y={0}>
+                            <Button className="button"><div className="fa fa-map-pin" aria-hidden="true"/></Button>
+                            <Button className="item1"><div className="fa fa-road" aria-hidden="true"/></Button>
+                            <Button className="item2"><div className="fa fa-info" aria-hidden="true"/></Button>
+                            <Button className="item2"><div className="fa fa-camera" aria-hidden="true"/></Button>
+                            <Button className="item2"><div className="fa fa-clock-o" aria-hidden="true"/></Button>
+                            <Button className="item2"><div className="fa fa-codepen" aria-hidden="true"/></Button>
+                            <Button className="item2"><div className="fa fa-wrench" aria-hidden="true"/></Button>
+                        </MotionMenu>
+                    </Pin>
+                </GoogleMapReact>
         );
     }
 }
