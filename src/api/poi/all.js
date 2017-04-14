9a47bff16d6c5d14aa1c7a651c7a77e3b195095f
TonyDC <up201305244@gcloud.fe.up.pt>
2017-04-14 22:08:54 +0100

    Params verification

--- src/api/poi/all.js
+++ src/api/poi/all.js
@@ -7,10 +7,39 @@ const router = express.Router();
 
 const db = root_require('models');
 
+router.get('/info/:id', (req, res) => {
+    const { id } = req.params;
+
+    if (!id) {
+        res.sendStatus(httpCodes.BAD_REQUEST).end();
+
+        return;
+    }
+
+    const POI = db.poi;
+    POI.findById(id).then((poi) => {
+        if (poi) {
+            res.json(poi.dataValues).end();
+        } else {
+            res.sendStatus(httpCodes.NOT_FOUND).end();
+        }
+    }).
+    catch((error) => {
+        console.error(error);
+        res.sendStatus(httpCodes.INTERNAL_SERVER_ERROR).end();
+    });
+});
+
+
 router.get('/range/:minLat/:maxLat/:minLng/:maxLng', (req, res) => {
     const { minLat, maxLat, minLng, maxLng } = req.params;
 
-    console.log(minLat, maxLat, minLng, maxLng);
+    if (!minLat || !maxLat || !minLng || !maxLng ||
+        isNaN(parseFloat(minLat)) || isNaN(parseFloat(maxLat)) || isNaN(parseFloat(minLng)) || isNaN(parseFloat(maxLng))) {
+        res.sendStatus(httpCodes.BAD_REQUEST).end();
+
+        return;
+    }
 
     const POI = db.poi;
     POI.findAll({
@@ -25,9 +54,13 @@ router.get('/range/:minLat/:maxLat/:minLng/:maxLng', (req, res) => {
             }
         }
     }).then((rows) => {
-        res.json(rows.map((item) => {
-            return item.dataValues;
-        })).end();
+        if (rows) {
+            res.json(rows.map((item) => {
+                return item.dataValues;
+            })).end();
+        } else {
+            res.sendStatus(httpCodes.NOT_FOUND).end();
+        }
     }).
     catch((error) => {
         console.error(error);
