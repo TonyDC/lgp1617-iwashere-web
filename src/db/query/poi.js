dcc91976f873bfea7d51f31c50e8761613d7c8e0
TonyDC <up201305244@gcloud.fe.up.pt>
2017-04-17 15:08:18 +0100

    SQL language injection

--- src/db/query/poi.js
+++ src/db/query/poi.js
@@ -3,6 +3,7 @@
 const db = require('../index');
 
 module.exports.getPOIDetail = (id) => {
+    // language=POSTGRES-SQL
     return db.query(`SELECT * FROM pois WHERE id = :id`, {
         replacements: { id },
         type: db.QueryTypes.SELECT
