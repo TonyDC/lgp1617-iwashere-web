0301ff9d39f7dfcf0a24c4ce7c3e3f6c80b43fbc
TonyDC <up201305244@gcloud.fe.up.pt>
2017-04-20 00:00:57 +0100

    DB query correction

--- src/db/query/poi.js
+++ src/db/query/poi.js
@@ -1,8 +1,5 @@
 "use strict";
 
-const moment = require('moment');
-const TODAY = moment().format();
-
 const db = require('../index');
 
 module.exports.getPOIDetailByID = (id) => {
@@ -57,10 +54,8 @@ module.exports.addPOIRating = (poiID, userID, rating) => {
     // language=POSTGRES-SQL
     return db.query(`INSERT INTO poi_ratings (poi_id, user_id, rating, "createdAt", "updatedAt") VALUES (:poiID, :userID, :rating, :createdAt, :updatedAt)`, {
         replacements: {
-            createdAt: TODAY,
             poiID,
             rating,
-            updatedAt: TODAY,
             userID
         },
         type: db.QueryTypes.INSERT
@@ -73,7 +68,6 @@ module.exports.updatePOIRating = (poiID, userID, rating) => {
         replacements: {
             poiID,
             rating,
-            updatedAt: TODAY,
             userID
         },
         type: db.QueryTypes.UPDATE
