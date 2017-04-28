e74d06f82d9005ee5ba8424deb0870766cb2e5eb
pmpontes <pedrompontes@outlook.com>
2017-04-28 02:05:58 +0100

    Lint validation

--- src/js/components/poi/POIPosts.js
+++ src/js/components/poi/POIPosts.js
@@ -140,7 +140,7 @@ export default class POIPosts extends Component {
             });
 
             if (this.componentIsMounted) {
-                this.setState({posts});
+                this.setState({ posts });
             }
         });
     }
