10c31b26cffab2ecd753182ecef56aeffa65440a
TonyDC <up201305244@gcloud.fe.up.pt>
2017-04-30 13:01:50 +0100

    Not found page

--- src/js/components/utils/NoMatch.js
+++ src/js/components/utils/NoMatch.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 
-import logo from 'img/logo.svg';
+import 'styles/utils.scss';
+import 'styles/not_found.scss';
 
 export default class NoMatch extends Component {
 
     render() {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <img alt="logo" className="App-logo" src={logo} />
+                <div className="wrapper-fill not-found-container">
+                    <div className="code-wrapper">
+                        <p id="code">4<span>0</span><span>4</span></p>
+                    </div>
+                    <div>
                     <h2>Oops!</h2>
                     <h5>The page you were looking for could not be found...</h5>
+                    </div>
                 </div>
-            </div>
         );
     }
 }
