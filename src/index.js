1c4cea6bed3605acc4ae51d6a22c74dd803d758e
TonyDC <up201305244@gcloud.fe.up.pt>
2017-05-28 11:36:11 +0100

    Checker refactoring

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import * as firebase from 'firebase';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from './js/redux/reducers';
 
 import App from './js/App';
-import Checker from './js/Checker';
 
 // Font Awesome
 import 'font-awesome-webpack';
@@ -21,16 +19,16 @@ import 'bootstrap-social';
 // Custom CSS
 import './styles/index.scss';
 
-// Initialize Firebase
-import { FIREBASE_CONFIG } from '../config';
-firebase.initializeApp(FIREBASE_CONFIG);
-
 // Material Design
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 // Inject onTouchTap event
 import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import { addReservedContexts } from 'js/redux/action creators/reserved';
+
+import Checker from 'js/Checker';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
@@ -39,12 +37,25 @@ injectTapEventPlugin();
 // Initialize Redux container
 const store = process.env.NODE_ENV === 'production' ? createStore(reducers) : createStore(reducers, applyMiddleware(logger));
 
-render(
-    <MuiThemeProvider muiTheme={getMuiTheme()}>
-    <Provider store={store}>
-        <Checker>
-            <App/>
-        </Checker>
-    </Provider>
-    </MuiThemeProvider>,
-    document.getElementById('main'));
+Checker.
+then((results) => {
+    const [contexts] = results;
+    if (contexts) {
+        const { availableContexts, selectedIndex } = contexts;
+        store.dispatch(addReservedContexts(availableContexts, selectedIndex));
+    }
+
+    // Render App
+    render(
+        <MuiThemeProvider muiTheme={getMuiTheme()}>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </MuiThemeProvider>,
+        document.getElementById('main'));
+}).
+catch((error) => {
+    console.log(error);
+});
+
+
--- src/js/Checker.js
+++ src/js/Checker.js
@@ -1,106 +1,53 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import nProgress from 'nprogress';
-import { GridLoader as Loader } from 'halogen';
-import firebase from 'firebase';
+import { FIREBASE_CONFIG } from '../../config';
+
+import * as firebase from 'firebase';
+const defaultApp = firebase.initializeApp(FIREBASE_CONFIG);
 
 import { authenticatedFetch, checkFetchResponse } from './functions/fetch';
-import { addReservedContexts } from './redux/action creators/reserved';
-
-import 'styles/utils.scss';
 
 const NO_ELEMENTS = 0;
 const ZERO_INDEX = 0;
 
+/*
+ * Note:
+ *  Firebase stores, in the local storage, information regarding the current logged in user.
+ *  Since Firebase has a delay as to confirm the identity of the user, it is required if a user is already logged in. Hence, the usage of the local storage.
+ */
+function checkFirebase() {
+    return new Promise((resolve, reject) => {
+        const { apiKey } = FIREBASE_CONFIG;
+        const localStorageProperty = `firebase:authUser:${apiKey}:[DEFAULT]`;
+        if (localStorage[localStorageProperty]) {
+            // The user is already logged in -> wait for Firebase refresh
+            const firebaseObserverUnsubscriber = defaultApp.auth().onAuthStateChanged((user) => {
+                if (user) {
+                    firebaseObserverUnsubscriber();
+
+                    authenticatedFetch('/api/reserved/user-type', {}, { 'Accept': 'application/json' }, 'GET').
+                    then(checkFetchResponse).
+                    then((contexts) => {
+                        let index = null;
+                        if (Array.isArray(contexts) && contexts.length > NO_ELEMENTS) {
+                            index = ZERO_INDEX;
+                        }
+                        resolve({
+                            availableContexts: contexts,
+                            selectedIndex: index
+                        });
+                    }).
+                    catch((error) => {
+                        reject(error);
+                    });
+                }
+            });
+        } else {
+            // No user is logged in
+            resolve(null);
+        }
+    });
+}
+
 /**
  * Ensures that a set of conditions are met before the application is rendered
  */
-export default class Checker extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = { allChecked: false };
-    }
-
-    componentDidMount() {
-        this.componentIsMounted = true;
-        this.checkConditions();
-    }
-
-    componentWillUnmount() {
-        this.componentIsMounted = false;
-    }
-
-    /*
-     * Note:
-     *  Firebase stores, in the local storage, information regarding the current logged in user.
-     *  Since Firebase has a delay as to confirm the identity of the user, it is required if a user is already logged in. Hence, the usage of the local storage.
-     */
-    checkFirebase() {
-        return new Promise((resolve, reject) => {
-            const localStorageProperty = `firebase:authUser:${firebase.app().options.apiKey}:[DEFAULT]`;
-            if (localStorage[localStorageProperty]) {
-                // The user is already logged in -> wait for Firebase refresh
-                this.firebaseObserverUnsubscriber = firebase.auth().onAuthStateChanged((user) => {
-                    if (user) {
-                        this.firebaseObserverUnsubscriber();
-
-                        // TODO change endpoint
-                        authenticatedFetch('/api/reserved/user-type', {}, { 'Accept': 'application/json' }, 'GET').
-                        then(checkFetchResponse).
-                        then((contexts) => {
-                            let index = null;
-                            if (Array.isArray(contexts) && contexts.length > NO_ELEMENTS) {
-                                index = ZERO_INDEX;
-                            }
-                            this.context.store.dispatch(addReservedContexts(contexts, index));
-                            resolve();
-                        }).
-                        catch((error) => {
-                            reject(error);
-                        });
-                    }
-                });
-            } else {
-                // No user is logged in
-                resolve();
-            }
-        });
-    }
-
-    checkConditions() {
-        nProgress.start();
-
-        Promise.all([this.checkFirebase()]).
-        then(() => {
-            this.setState({ allChecked: true });
-        }).
-        catch(() => {
-            this.setState({ allChecked: true });
-        }).
-        then(() => {
-            nProgress.done();
-        });
-    }
-
-    render() {
-        const { allChecked } = this.state;
-        if (allChecked) {
-            // Only one child is permitted, otherwise, React considers it as an error
-            return this.props.children;
-        }
-
-        return (
-            <div className="hor-align vert-align">
-                <Loader size="40px" color="#FF6633"/>
-            </div>
-        );
-    }
-}
-
-Checker.propTypes = { children: PropTypes.any };
-
-Checker.contextTypes = {
-    muiTheme: PropTypes.object,
-    store: PropTypes.object
-};
+export default Promise.all([checkFirebase()]);
