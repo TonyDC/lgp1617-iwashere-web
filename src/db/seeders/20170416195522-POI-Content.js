4979d1df96931208a774c1a36e295537ca784c89
aramiis <andrecmmaia@gmail.com>
2017-05-30 12:11:22 +0100

    db some images and gifs

--- src/db/seeders/20170416195522-POI-Content.js
+++ src/db/seeders/20170416195522-POI-Content.js
@@ -25,11 +25,147 @@ module.exports = {
             },
             {
                 content_type_id: 1,
-                poi_id: 44,
-                url_l: 'initial-content/44/11.png',
-                url_m: 'initial-content/44/11.png',
-                url_s: 'initial-content/44/11.png',
-                url_xs: 'initial-content/44/11.png'
+                poi_id: 139,
+                url_l: 'initial-content/139/Poente na Serra_l.png',
+                url_m: 'initial-content/139/Poente na Serra_m.png',
+                url_s: 'initial-content/139/Poente na Serra_s.png',
+                url_xs: 'initial-content/139/Poente na Serra_xs.png'
+            },
+            {
+                content_type_id: 1,
+                poi_id: 140,
+                url_l: 'initial-content/140/Perspetiva de cinema_l.png',
+                url_m: 'initial-content/140/Perspetiva de cinema_m.png',
+                url_s: 'initial-content/140/Perspetiva de cinema_s.png',
+                url_xs: 'initial-content/140/Perspetiva de cinema_xs.png'
+            },
+            {
+                content_type_id: 1,
+                poi_id: 141,
+                url_l: 'initial-content/141/Ovario de coelha_l.png',
+                url_m: 'initial-content/141/Ovario de coelha_m.png',
+                url_s: 'initial-content/141/Ovario de coelha_s.png',
+                url_xs: 'initial-content/141/Ovario de coelha_xs.png'
+            },
+            {
+                content_type_id: 1,
+                poi_id: 142,
+                url_l: 'initial-content/142/Disco_l.gif',
+                url_m: 'initial-content/142/Disco_m.gif',
+                url_s: 'initial-content/142/Disco_s.gif',
+                url_xs: 'initial-content/142/Disco_xs.gif'
+            },
+            {
+                content_type_id: 1,
+                poi_id: 143,
+                url_l: 'initial-content/143/Sela_l.gif',
+                url_m: 'initial-content/143/Sela_m.gif',
+                url_s: 'initial-content/143/Sela_s.gif',
+                url_xs: 'initial-content/143/Sela_xs.gif'
+            },
+            {
+                content_type_id: 1,
+                poi_id: 144,
+                url_l: 'initial-content/144/Circulo de reflexao_l.gif',
+                url_m: 'initial-content/144/Circulo de reflexao_m.gif',
+                url_s: 'initial-content/144/Circulo de reflexao_s.gif',
+                url_xs: 'initial-content/144/Circulo de reflexao_xs.gif'
+            },
+            {
+                content_type_id: 1,
+                poi_id: 148,
+                url_l: 'initial-content/148/Moinho_l.gif',
+                url_m: 'initial-content/148/Moinho_m.gif',
+                url_s: 'initial-content/148/Moinho_s.gif',
+                url_xs: 'initial-content/148/Moinho_xs.gif'
+            },
+            {
+                content_type_id: 1,
+                poi_id: 149,
+                url_l: 'initial-content/149/Pipeta_l.png',
+                url_m: 'initial-content/149/Pipeta_m.png',
+                url_s: 'initial-content/149/Pipeta_s.png',
+                url_xs: 'initial-content/149/Pipeta_xs.png'
+            },
+            {
+                content_type_id: 1,
+                poi_id: 151,
+                url_l: 'initial-content/151/Povo e Tropa_l.gif',
+                url_m: 'initial-content/151/Povo e Tropa_m.gif',
+                url_s: 'initial-content/151/Povo e Tropa_s.gif',
+                url_xs: 'initial-content/151/Povo e Tropa_xs.gif'
+            },
+            {
+                content_type_id: 1,
+                poi_id: 153,
+                url_l: 'initial-content/153/Mulher gravida e instrumento medico_1_l.png',
+                url_m: 'initial-content/153/Mulher gravida e instrumento medico_1_m.png',
+                url_s: 'initial-content/153/Mulher gravida e instrumento medico_1_s.png',
+                url_xs: 'initial-content/153/Mulher gravida e instrumento medico_1_xs.png'
+            },
+            {
+                content_type_id: 1,
+                poi_id: 153,
+                url_l: 'initial-content/153/Mulher gravida e instrumento medico_2_l.png',
+                url_m: 'initial-content/153/Mulher gravida e instrumento medico_2_m.png',
+                url_s: 'initial-content/153/Mulher gravida e instrumento medico_2_s.png',
+                url_xs: 'initial-content/153/Mulher gravida e instrumento medico_2_xs.png'
+            },
+            {
+                content_type_id: 1,
+                poi_id: 153,
+                url_l: 'initial-content/153/Mulher gravida e instrumento medico_3_l.png',
+                url_m: 'initial-content/153/Mulher gravida e instrumento medico_3_m.png',
+                url_s: 'initial-content/153/Mulher gravida e instrumento medico_3_s.png',
+                url_xs: 'initial-content/153/Mulher gravida e instrumento medico_3_xs.png'
+            },
+            {
+                content_type_id: 1,
+                poi_id: 154,
+                url_l: 'initial-content/154/Estojo instrumentos medicos_1_l.png',
+                url_m: 'initial-content/154/Estojo instrumentos medicos_1_m.png',
+                url_s: 'initial-content/154/Estojo instrumentos medicos_1_s.png',
+                url_xs: 'initial-content/154/Estojo instrumentos medicos_1_xs.png'
+            },
+            {
+                content_type_id: 1,
+                poi_id: 154,
+                url_l: 'initial-content/154/Estojo instrumentos medicos_2_l.png',
+                url_m: 'initial-content/154/Estojo instrumentos medicos_2_m.png',
+                url_s: 'initial-content/154/Estojo instrumentos medicos_2_s.png',
+                url_xs: 'initial-content/154/Estojo instrumentos medicos_2_xs.png'
+            },
+            {
+                content_type_id: 1,
+                poi_id: 155,
+                url_l: 'initial-content/155/Rosto em cera_l.gif',
+                url_m: 'initial-content/155/Rosto em cera_m.gif',
+                url_s: 'initial-content/155/Rosto em cera_s.gif',
+                url_xs: 'initial-content/155/Rosto em cera_xs.gif'
+            },
+            {
+                content_type_id: 1,
+                poi_id: 156,
+                url_l: 'initial-content/156/heart_l.gif',
+                url_m: 'initial-content/156/heart_m.gif',
+                url_s: 'initial-content/156/heart_s.gif',
+                url_xs: 'initial-content/156/heart_xs.gif'
+            },
+            {
+                content_type_id: 1,
+                poi_id: 160,
+                url_l: 'initial-content/160/Fonografo de Edison_l.gif',
+                url_m: 'initial-content/160/Fonografo de Edison_m.gif',
+                url_s: 'initial-content/160/Fonografo de Edison_s.gif',
+                url_xs: 'initial-content/160/Fonografo de Edison_xs.gif'
+            },
+            {
+                content_type_id: 1,
+                poi_id: 165,
+                url_l: 'initial-content/165/Faina fluvial no Douro_l.png',
+                url_m: 'initial-content/165/Faina fluvial no Douro_m.png',
+                url_s: 'initial-content/165/Faina fluvial no Douro_s.png',
+                url_xs: 'initial-content/165/Faina fluvial no Douro_xs.png'
             }
         ], {});
     }
