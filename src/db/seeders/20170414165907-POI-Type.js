6e24976e59db2fbe70ae0941ee3bfcf656ce4da8
aramiis <andrecmmaia@gmail.com>
2017-05-30 02:03:29 +0100

    db people and more streets, minor text fixes

--- src/db/seeders/20170414165907-POI-Type.js
+++ src/db/seeders/20170414165907-POI-Type.js
@@ -6,9 +6,9 @@ module.exports = {
     },
     up: (queryInterface) => {
         return queryInterface.bulkInsert('poi_types', [
+            { name: 'person;pessoa' },
             { name: 'place;lugar' },
             { name: 'object;objeto' }
-
         ], {});
     }
 };
