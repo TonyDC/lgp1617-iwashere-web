a51020469c31b1d2821a13e76be8e0962e22e619
TonyDC <up201305244@gcloud.fe.up.pt>
2017-06-12 15:02:06 +0100

    On-demand code splitting

--- src/js/App.js
+++ src/js/App.js
@@ -3,18 +3,52 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Router, Route, IndexRoute, IndexRedirect, browserHistory } from 'react-router';
+import Async from 'react-code-splitting';
 import * as firebase from 'firebase';
 
-import AdminShell from './components/utils/AdminShell';
+// import AdminShell from './components/utils/AdminShell';
+
+const AdminShell = (props) => {
+    return <Async load={ import('./components/utils/AdminShell') } componentProps={props}/>;
+};
+const POIArea = (props) => {
+    return <Async load={ import('./components/reserved/admin/poi/POIArea') } componentProps={props}/>;
+};
+const CreatePOI = (props) => {
+    return <Async load={ import('./components/reserved/admin/poi/CreatePOI') } componentProps={props}/>;
+};
+const EditPOI = (props) => {
+    return <Async load={ import('./components/reserved/admin/poi/EditPOI') } componentProps={props}/>;
+};
+const RouteArea = (props) => {
+    return <Async load={ import('./components/reserved/admin/routes/RouteArea') } componentProps={props}/>;
+};
+const CreateRoute = (props) => {
+    return <Async load={ import('./components/reserved/admin/routes/CreateRoute') } componentProps={props}/>;
+};
+const EditRoute = (props) => {
+    return <Async load={ import('./components/reserved/admin/routes/EditRoute') } componentProps={props}/>;
+};
+
+
+const Register = (props) => {
+    return <Async load={ import('./components/user/Register') } componentProps={props}/>;
+};
+const PasswordReset = (props) => {
+    return <Async load={ import('./components/user/PasswordReset') } componentProps={props}/>;
+};
+
 
 import MainRoutes from './routes/MainRoutes';
-
 import NoMatch from './components/utils/NoMatch';
 
 import Map from './components/map/Map';
+
 import Login from './components/user/Login';
+/*
 import Register from './components/user/Register';
 import PasswordReset from './components/user/PasswordReset';
+*/
 import POIDetail from './components/poi/POIDetail';
 import RouteDetail from './components/route/RouteDetail';
 import Search from './components/search/Search';
@@ -24,13 +58,14 @@ import POICreatePost from './components/poi/POICreatePost';
 import UnauthRoutes from './routes/UnauthRoutes';
 import POIRoutes from './routes/POIRoutes';
 import RouteRoutes from './routes/RouteRoutes';
-
+/*
 import POIArea from './components/reserved/admin/poi/POIArea';
 import CreatePOI from './components/reserved/admin/poi/CreatePOI';
 import EditPOI from './components/reserved/admin/poi/EditPOI';
 import RouteArea from './components/reserved/admin/routes/RouteArea';
 import CreateRoute from './components/reserved/admin/routes/CreateRoute';
 import EditRoute from './components/reserved/admin/routes/EditRoute';
+*/
 // import UserArea from './components/reserved/admin/users/UserArea';
 // import CreateUser from './components/reserved/admin/users/CreateUser';
 // import EditUser from './components/reserved/admin/users/EditUser';
