60d8d2a21a256b497c517624f548d741d3c6c497
TonyDC <up201305244@gcloud.fe.up.pt>
2017-06-15 13:40:12 +0100

    webpack.dev correction

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,10 +4,10 @@ const webpack = require('webpack');
 module.exports = {
     devtool: 'eval',
 
-    entry: [
-        'webpack-hot-middleware/client',
-        './src/index'
-    ],
+    entry: {
+        app: ['webpack-hot-middleware/client', './src/index'],
+        vendor: ['webpack-hot-middleware/client', 'react', 'react-dom', 'moment', 'material-ui']
+    },
 
     output: {
         path: path.join(__dirname, 'public'),
@@ -26,9 +26,13 @@ module.exports = {
             'process.env': {
                 'NODE_ENV': JSON.stringify('development')                       // This is required for the following: src/index.js imports a CommonJS-compliant file, where it it analyzed
             }                                                                   // the 'process.env' property. For the NodeJS, there is no problem because the environment variable is set
-        })                                                                      // when the command is run. However, when a ES6-compliant file accesses the same file (config/index.js), the
-    ],                                                                          // environment variables are not set, because they are transpiled by Webpack. As such, it is necessary to define
+        }),                                                                     // when the command is run. However, when a ES6-compliant file accesses the same file (config/index.js), the
+                                                                                // environment variables are not set, because they are transpiled by Webpack. As such, it is necessary to define
                                                                                 // the environment variable using webpack.DefinePlugin.
+
+        new webpack.optimize.CommonsChunkPlugin({ name: "vendor", filename: "vendor.bundle.js" })
+    ],
+
     resolve: {
         modules: [
             path.join(__dirname, "src"),
