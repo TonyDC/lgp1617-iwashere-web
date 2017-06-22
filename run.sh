3427e4f11efe94ee0356301cd8d42f783c70c958
TonyDC <up201305244@gcloud.fe.up.pt>
2017-06-22 01:56:09 +0100

    Script: added 'gulp' command

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@
 npm install &&
 ./node_modules/.bin/sequelize db:migrate db:migrate &&
 NODE_ENV=production webpack --config ./webpack.prod.config.js --progress --colors &&
+NODE_ENV=production gulp &&
 NODE_ENV=production node ./app.js
